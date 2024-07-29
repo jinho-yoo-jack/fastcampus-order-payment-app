fb850ad0c971e023847009097bdb575c0346084c
jinho-yoo-jack <jinho@jinhoui-Macmini.local>
2024-07-29 16:21:20 +0900

    ADD: AWS

--- docker/kafka/kafka-stop.sh
+++ docker/kafka/kafka-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ZK=docker-zookeeper-1
-BROKER=docker-kafka-1
+BROKER=docker-kafka-1-1
 REGISTRY=docker-schema-registry-1
 docker stop $ZK && docker rm $ZK
 docker stop $BROKER && docker rm $BROKER
